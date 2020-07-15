2cc3d07b0a57e836c00c289d6b0c00b8f77e4884
Igor da Costa Paschoal Vidotto Felipe <igor_vidotto@hotmail.com>
2020-07-15 18:10:21 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './screens/products_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Playground',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsScreen(),
+    );
+  }
+}
+
